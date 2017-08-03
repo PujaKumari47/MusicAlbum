96b7904df587b24902b0ddc53cca0d96570c1994
PujaKumari47 <puja@nineleaps.com>
2017-08-03 17:59:12 +0530

    First commit

---
+++ MusicAlbum/MusicCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MusicCollectionViewCell.swift
+//  MusicAlbum
+//
+//  Created by Puja Kumari on 8/3/17.
+//  Copyright © 2017 nineleaps. All rights reserved.
+//
+
+import UIKit
+
+class MusicCollectionViewCell: UICollectionViewCell {
+    
+}
